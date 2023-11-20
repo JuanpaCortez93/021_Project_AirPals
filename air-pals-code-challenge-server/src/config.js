8e86c7a613f46fe80f383fd2191ee25072669db8
JuanpaCortez93 <63519906+JuanpaCortez93@users.noreply.github.com>
2023-11-19 21:40:34 -0500

    [update] settings.js

--- air-pals-code-challenge-server/src/config.js
+++ air-pals-code-challenge-server/src/config.js
@@ -16,4 +16,4 @@ export const DB_PORT = process.env.DB_PORT || 3306;
 export const DB_NAME = process.env.DB_NAME || 'zipcodedb';
 
 //GOOGLE MAPS API
-export const GOOGLE_MAPS_API = process.env.GOOGLE_MAPS_API || '';
+export const GOOGLE_MAPS_API = process.env.GOOGLE_MAPS_API || 'AIzaSyC7V4A5k_CUlnlZS9u5EN6TC3mm1JLZQE0';
